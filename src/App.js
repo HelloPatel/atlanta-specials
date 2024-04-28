c0752922526c6fe3f8a27da9fd907952eab5d447
Rushi Patel <patel.rushi512@gmail.com>
2024-04-28 09:59:09 -0400

    hello
    he

--- src/App.js
+++ src/App.js
@@ -1,54 +1,65 @@
+// App.js
+
 import React, { useState } from 'react';
 import './App.css'; // Import CSS file for styling
 import restaurantImage from './img/restaurant-image.jpg'; // Import your restaurant image
+import { daysOfWeek, locationOptions, cuisineOptions, restaurantsList } from './common/commonComponents'; // Import common function and properties
+import Select from 'react-select'; // Import react-select component
 
 function App() {
   // Sample restaurant data
-  const restaurants = [
-    { name: 'Postinos', location: ['Buckhead', 'West Midtown'], cuisine: 'American', specials: { Monday: '$25 for wine and bruschetta board after 8PM', Tuesday: '$25 for wine and bruschetta board after 8PM', Wednesday: '', Thursday: '', Friday: '', Saturday: 'Brunch', Sunday: 'Brunch' }, website: 'https://www.postinowinecafe.com' },
-    { name: 'Pachengos', location: ['Buckhead'], cuisine: 'Mexican', specials: { Monday: '', Tuesday: 'Dive into the deliciousness of $2 Taco Tuesday, all day long! Indulge in mouthwatering options including pollo, asada, pastor, and rajas (veggie). Please note, that this special is exclusive for dine-in only.', Wednesday: 'Calling all industry folks! It\'s Industry Night at Pachengo! Enjoy $5 house tequila shots and complimentary chips & salsa from 5 pm to closing.', Thursday: 'Ladies, gather your squad for Tequila Thursday! Sip on $9 Margaritas, $5 Beer, and $8 Wine from 5 pm to closing. ', Friday: 'Get ready to groove at Pachengo\'s Latino Night! From 6 pm to 10 pm, a live DJ will set the perfect vibe. Plus, enjoy free chips & salsa from 9 pm until closing. It\'s a weekend party you won\'t want to miss!', Saturday: 'Get ready to groove at Pachengo\'s Latino Night! From 6 pm to 10 pm, a live DJ will set the perfect vibe. Plus, enjoy free chips & salsa from 9 pm until closing. It\'s a weekend party you won\'t want to miss!', Sunday: 'Sunday Brunch' }, website: 'https://www.pachengos.com/' },
-    { name: 'Restaurant C', location: ['Atlanta'], cuisine: 'American', specials: { Monday: 'Burger Monday', Tuesday: 'Steak Tuesday', Wednesday: 'Seafood Wednesday', Thursday: 'Wine Thursday', Friday: 'Fish Fryday', Saturday: 'BBQ Night', Sunday: 'Family Dinner' }, website: 'https://restaurantC.com' },
-    // Add more restaurant objects as needed
-  ];
-
-  // Options for location filter dropdown
-  const locationOptions = ['All', 'Atlanta', 'New York', 'Los Angeles', 'Chicago']; // Sample options
-
-  // Options for cuisine filter dropdown
-  const cuisineOptions = ['All', 'Italian', 'Mexican', 'American', 'Japanese']; // Sample options
-
-  // Days of the week
-  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const restaurants = restaurantsList;
 
   // States for filters
-  const [locationFilter, setLocationFilter] = useState('All');
-  const [cuisineFilter, setCuisineFilter] = useState('All');
+  const [locationFilter, setLocationFilter] = useState([]);
+  const [cuisineFilter, setCuisineFilter] = useState([]);
+  const [selectedDay, setSelectedDay] = useState([]);
 
-  // Filtered restaurants based on location and cuisine
+  // Convert options to react-select format
+  const locationOptionsFormatted = locationOptions.map(option => ({ value: option, label: option }));
+  const cuisineOptionsFormatted = cuisineOptions.map(option => ({ value: option, label: option }));
+  const daysOfWeekFormatted = daysOfWeek.map(day => ({ value: day, label: day }));
+
+  // Filtered restaurants based on location, cuisine, and selected day
   const filteredRestaurants = restaurants.filter((restaurant) => {
     return (
-      (locationFilter === 'All' || restaurant.location.includes(locationFilter)) &&
-      (cuisineFilter === 'All' || restaurant.cuisine === cuisineFilter)
+      (locationFilter.length === 0 || locationFilter.some(option => restaurant.location.includes(option.value))) &&
+      (cuisineFilter.length === 0 || cuisineFilter.some(option => restaurant.cuisine === option.value)) &&
+      (selectedDay.length === 0 || selectedDay.some(option => restaurant.specials[option.value] !== undefined))
     );
   });
 
   return (
     <div className="container">
-      <h1 className="title">Atlanta Specials Review</h1>
-      <img src={restaurantImage} alt="Restaurant" className="restaurant-image" />
+      <h1 className="title">Atlanta Specials</h1>
       <div className="filters">
-        <label>Location Filter:</label>
-        <select value={locationFilter} onChange={(e) => setLocationFilter(e.target.value)}>
-          {locationOptions.map((option, index) => (
-            <option key={index} value={option}>{option}</option>
-          ))}
-        </select>
-        <label>Cuisine Type Filter:</label>
-        <select value={cuisineFilter} onChange={(e) => setCuisineFilter(e.target.value)}>
-          {cuisineOptions.map((option, index) => (
-            <option key={index} value={option}>{option}</option>
-          ))}
-        </select>
+        <label>Location:</label>
+        <div className="select-wrapper">
+          <Select
+            options={locationOptionsFormatted}
+            value={locationFilter}
+            onChange={(selectedOptions) => setLocationFilter(selectedOptions)}
+            isMulti
+          />
+        </div>
+        <label>Cuisine Type:</label>
+        <div className="select-wrapper">
+          <Select
+            options={cuisineOptionsFormatted}
+            value={cuisineFilter}
+            onChange={(selectedOptions) => setCuisineFilter(selectedOptions)}
+            isMulti
+          />
+        </div>
+        <label>Day of the Week:</label>
+        <div className="select-wrapper">
+          <Select
+            options={daysOfWeekFormatted}
+            value={selectedDay}
+            onChange={(selectedOptions) => setSelectedDay(selectedOptions)}
+            isMulti
+          />
+        </div>
       </div>
       <div className="table-container">
         <table className="restaurant-table">
@@ -70,7 +81,7 @@ function App() {
                   <table className="specials-table">
                     <tbody>
                       {daysOfWeek.map((day) => (
-                        <tr key={day}>
+                        <tr key={day} style={{ display: selectedDay.length === 0 || selectedDay.some(option => option.value === day) ? 'table-row' : 'none' }}>
                           <td className="day-of-week">{day}</td>
                           <td>{restaurant.specials[day]}</td>
                         </tr>
---
+++ src/common/commonComponents.js
@@ -0,0 +1,84 @@
+// restaurantUtils.js
+
+import { BlobServiceClient } from "@azure/storage-blob"
+import React, { useEffect, useState } from "react";
+
+// Function to create a restaurant object
+function createRestaurant(name, locations, cuisine, specials, website) {
+    return {
+        name: name,
+        location: locations,
+        cuisine: cuisine,
+        specials: specials,
+        website: website
+    };
+}
+
+// Static properties
+export const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+export const restaurantsList = [
+    createRestaurant(
+      'Postinos',
+      ['Buckhead', 'West Midtown'],
+      'American',
+      { Monday: '$25 for wine and bruschetta board after 8PM', Tuesday: '$25 for wine and bruschetta board after 8PM', Wednesday: '', Thursday: '', Friday: '', Saturday: 'Brunch', Sunday: 'Brunch' },
+      'https://www.postinowinecafe.com'
+    ),
+    createRestaurant(
+      'Pachengos',
+      ['Buckhead'],
+      'Mexican',
+      { Monday: '', Tuesday: 'Dive into the deliciousness of $2 Taco Tuesday, all day long! Indulge in mouthwatering options including pollo, asada, pastor, and rajas (veggie). Please note, that this special is exclusive for dine-in only.', Wednesday: 'Calling all industry folks! It\'s Industry Night at Pachengo! Enjoy $5 house tequila shots and complimentary chips & salsa from 5 pm to closing.', Thursday: 'Ladies, gather your squad for Tequila Thursday! Sip on $9 Margaritas, $5 Beer, and $8 Wine from 5 pm to closing. ', Friday: 'Get ready to groove at Pachengo\'s Latino Night! From 6 pm to 10 pm, a live DJ will set the perfect vibe. Plus, enjoy free chips & salsa from 9 pm until closing. It\'s a weekend party you won\'t want to miss!', Saturday: 'Get ready to groove at Pachengo\'s Latino Night! From 6 pm to 10 pm, a live DJ will set the perfect vibe. Plus, enjoy free chips & salsa from 9 pm until closing. It\'s a weekend party you won\'t want to miss!', Sunday: 'Sunday Brunch' },
+      'https://www.pachengos.com/'
+    ),
+    createRestaurant(
+      'Gypsy Kitchen',
+      ['Buckhead'],
+      'American',
+      { Monday: 'Happy Hour 4pm-6pm', Tuesday: 'Happy Hour 4pm-6pm', Wednesday: 'Happy Hour 4pm-6pm', Thursday: 'Happy Hour 4pm-6pm', Friday: 'Happy Hour 4pm-6pm', Saturday: 'Brunch', Sunday: 'Brunch' },
+      'https://static1.squarespace.com/static/53d80c4ae4b0326a80e22518/t/6321fec22ef3691d15b6d9ee/1663172292262/GKATL-HAPPYHR-web.pdf'
+    ),
+    createRestaurant(
+        'The Iberian Pig',
+        ['Buckhead', 'Decatur'],
+        'Mexican',
+        { Monday: 'Happy Hour 5pm-6pm', Tuesday: 'Happy Hour 5pm-6pm', Wednesday: 'Happy Hour 5pm-6pm', Thursday: 'Happy Hour 5pm-6pm', Friday: 'Happy Hour 4pm-6pm', Saturday: 'Happy Hour 4pm-6pm', Sunday: 'Happy Hour 5pm-6pm' },
+        'hhttps://static1.squarespace.com/static/5cb0fcadda50d3755d9e0e77/t/661d5f017d267a1e1d79c079/1713200897801/Happy+Hour+NEW+2024-04-10.pdf'
+      ),
+      createRestaurant(
+        'Industry Tavern',
+        ['Buckhead'],
+        'American',
+        { Monday: 'Mix\'ed Up Monday! $6 EL JIMADOR $5 DOS XX LAGER', Tuesday: 'Tito’s Tuesday! $5 TITO’S VODKA', Wednesday: 'Whiskey Wednesday! $6 JACK DANIELS WHISKEY. Dirty South Trivia 8pm', Thursday: 'Throwback Thursday! $3.5 HIGH LIFE & COORS BANQUET', Friday: 'Forrester Friday! $5 TWISTED TEA WHISKEY $5 TWISTED TEA LITE CANS', Saturday: 'Craft Saturday! $6 CREATURE COMFORTS DRAFT ', Sunday: 'Sunday Funday! HALF PRICED HIGH NOON CANS' },
+        'https://www.industrytavern.com/weekly-specials/'
+      ),
+      createRestaurant(
+        'Eclipse di luna',
+        ['Buckhead', 'Dunwoody', 'Alpharetta'],
+        'American',
+        { Monday: 'Happy Hour: $5 Tapas & $5 Margaritas, Mojitos, Sangria, Well Liquor, Beer & Wine (4pm to 6pm) – 1/2 off all bottles of wine', Tuesday: 'Happy Hour: $5 Tapas & $5 Margaritas, Mojitos, Sangria, Well Liquor, Beer & Wine (4pm to 6pm) – $5 Red & White Sangria All Day', Wednesday: 'Happy Hour: $5 Tapas & $5 Margaritas, Mojitos, Sangria, Well Liquor, Beer & Wine (4pm to 6pm) – Half Off All Paellas All Night', Thursday: 'Happy Hour: $5 Tapas & $5 Margaritas, Mojitos, Sangria, Well Liquor, Beer & Wine (4pm to 6pm)', Friday: 'Happy Hour: $5 Tapas & $5 Margaritas, Mojitos, Sangria, Well Liquor, Beer & Wine (4pm to 6pm)', Saturday: '', Sunday: 'Brunch: Bottomless Mimosas & Sangria (12pm to 4pm) + Full Menu All Day' },
+        'https://eclipsediluna.com/buckhead-deal/'
+      ),
+      
+    // Add more restaurant objects as needed
+  ];
+
+const allLocations = restaurantsList.reduce((locations, restaurant) => {
+    restaurant.location.forEach(location => {
+        if (!locations.includes(location)) {
+            locations.push(location);
+        }
+    });
+    return locations;
+}, []);
+
+export const locationOptions = ['All', ...allLocations];
+
+
+export const cuisineOptions = ['All', 'Italian', 'Mexican', 'American', 'Japanese'];
+
+
+
+
+
